4b63e56f3da1f1ccc3be0638563e5a0c2d225349
tt-213268 <takalkartejastt@gmail.com>
2022-11-17 09:23:30 +0100

    index.js:-  modified to send a json object instead of hello world

--- user_service_api/index.js
+++ user_service_api/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => res.json({     //we modified this to send a json object instead of hello world
+  name: 'Bob',
+  email: 'bob@gmail.com'
+}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
